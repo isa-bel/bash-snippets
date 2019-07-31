2a3f7b3d5e9f59bc818aa545969520632af06ca3
Isabel <isa-bel@users.noreply.github.com>
2019-07-31 18:07:08 -0500

    Add ngrok function

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 Useful bash scripts
 
 * [Dependencies](./dependencies.sh) Functions to test if a lib is installed. Useful to create an install script for your project.
+* [ngrok path](./ngrok-path.sh) Gets the path where [ngrok](https://ngrok.com/) is running.
+
+## Bash Tips
+
+* Use emoji
---
+++ ngrok-path.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# Requirements:
+# * jq https://stedolan.github.io/jq/
+
+# Returns the path where ngrok is running
+# Example: getNgrokPath
+function getNgrokPath {
+  echo '🔎 Verify that ngrok is running'
+
+  NGROK_PATH=$(curl http://localhost:4040/api/tunnels | jq -r '.tunnels[0].public_url')
+
+  if [[ -z ${NGROK_PATH} ]]; then
+    echo '😟 ngrok is not running'
+    return 0
+  fi
+
+  echo "🚇 ngrok running at ${NGROK_PATH}"
+
+  return ${NGROK_PATH}
+}
+
